db69ecb07b739a3ab2d4d85b68abc41a580ede2e
charles.aldarondo@gmail.com <moriah9879>
2014-11-08 10:57:10 -0700

    Initial Version

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name excelToJsonApp
+ * @description
+ * # excelToJsonApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('excelToJsonApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
